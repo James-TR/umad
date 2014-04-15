4b81ab39ccf7f407c59cf95a260841550a6ba712
Barney Desmond <barney.desmond@anchor.net.au>
2014-04-15 17:00:26 +1000

    Use a more likely default for the testing listener endpoint

--- testing/enqueue_urls.py
+++ testing/enqueue_urls.py
@@ -7,7 +7,7 @@ import requests
 parser = argparse.ArgumentParser(description="Read URLs from stdin or files, and enqueue them for indexing/deletion")
 parser.add_argument('input', type=argparse.FileType('r'), default=[sys.stdin], nargs='*', metavar="filename", help="Input file/s, defaults to stdin (-)")
 parser.add_argument('-d', '--delete', action="store_true", help="Enqueue URLs for deletion instead of re/indexing")
-parser.add_argument('--listener', default='http://localhost:8081/', help="URL of the UMAD listener [default: %(default)s]")
+parser.add_argument('--listener', default='https://umad-indexer.anchor.net.au/', help="URL of the UMAD listener [default: %(default)s]")
 args = parser.parse_args()
 
 request_method = requests.delete if args.delete else requests.get
