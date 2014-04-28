c5687f38e4410026bf5ca623c78e43006e279390
Sarah Kowalik <sarah.kowalik@anchor.com.au>
2014-04-28 12:41:35 +1000

    Url changed

--- update_domains.py
+++ update_domains.py
@@ -6,7 +6,7 @@ def main():
 	distiller = DomainDistiller(None)
 	domain_list = distiller.get_domain_list()
 	for domain in domain_list:
-		r = requests.get("https://umad-indexer.stg.anchor.net.au", params={'url':'https://domains.anchor.com.au/{0}'.format(domain)}, verify=False)
+		r = requests.get("https://umad-indexer-stg.anchor.net.au", params={'url':'https://domains.anchor.com.au/{0}'.format(domain)}, verify=False)
 		print r.text
 
 if __name__ == "__main__":
