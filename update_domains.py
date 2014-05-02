76b43ec54577a51bb0076d47200085c620c1a2af
Barney Desmond <barney.desmond@anchor.net.au>
2014-05-02 16:09:27 +1000

    update_domains.py should hit production indexer by default, and can now take an alternate UMAD_INDEXER_URL
    This is an important change because update_domains.py is run in
    production every 10min via cron, and we need it to keep the index up to
    date.

--- update_domains.py
+++ update_domains.py
@@ -1,12 +1,15 @@
-from distil.domain import DomainDistiller
 import sys
+import os
 import requests
+from distil.domain import DomainDistiller
+
+UMAD_INDEXER_URL = os.environ.get('UMAD_INDEXER_URL', 'https://umad-indexer.anchor.net.au/')
 
 def main():
 	distiller = DomainDistiller(None)
 	domain_list = distiller.get_domain_list()
 	for domain in domain_list:
-		r = requests.get("https://umad-indexer-stg.anchor.net.au", params={'url':'https://domains.anchor.com.au/{0}'.format(domain)}, verify=False)
+		r = requests.get(UMAD_INDEXER_URL, params={'url':'https://domains.anchor.com.au/{0}'.format(domain)}, verify=False)
 		print r.text
 
 if __name__ == "__main__":
