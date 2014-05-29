3fd80601e90283f6b14487e3cadc78d2a293e141
Sarah Kowalik <sarah.kowalik@anchor.com.au>
2014-05-29 19:03:57 +1000

    Handle variable correctly

--- indexing_worker/indexing_worker.py
+++ indexing_worker/indexing_worker.py
@@ -17,8 +17,7 @@ def mention(msg):
 	sys.stderr.flush()
 
 
-DEBUG = os.environ.get('UMAD_INDEXING_WORKER_DEBUG')
-DEBUG = True
+DEBUG = os.environ.get('UMAD_INDEXING_WORKER_DEBUG', True)
 PID_PREFIX = '[pid {0}] '.format(os.getpid())
 debug("Debug logging is enabled")
 
